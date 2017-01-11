d5f27fde9b608834202eebb4a64c01c6626c15b0
ruslanprikhdk <ruslanprikhdk@gmail.com>
2017-01-11 11:28:04 +0200

    Add files via upload

---
+++ Form/js/script.js
@@ -0,0 +1,27 @@
+﻿$(function() {
+  $('.comment1').hide();
+  $('.comment2').hide();
+  $('.comment3').hide();
+  $('.firstname_p').hover(function() {
+    $('.comment1').fadeIn(600).show();
+    }, function(){
+    $('.comment1').hide();
+  });
+  $('.lastname_p').hover(function() {
+    $('.comment2').fadeIn(600).show();
+    }, function(){
+    $('.comment2').hide();
+  });
+  $('.address_p').hover(function() {
+    $('.comment3').fadeIn(600).show();
+    }, function(){
+    $('.comment3').hide();
+  });
+  $('.button').click(function() {
+    $('.comment1').fadeIn(600).show();
+    $('.comment2').fadeIn(600).show();
+    $('.comment3').fadeIn(600).show();
+  });
+});
+
+
---
+++ Tabs/js/script.js
@@ -0,0 +1,10 @@
+﻿$(function() {
+  $('.tabs .tab-links a').on('click', function(e) {
+    var $currentAttrValue = $(this).attr('href');
+    $('.tabs ' + $currentAttrValue).fadeIn(400).siblings().hide();
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
+    e.preventDefault();
+    });
+});
+
+
